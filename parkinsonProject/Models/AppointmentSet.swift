213597621a63db92b2c5be8bdd3a689a067c667d
FlorentBerland <florent.berland@laposte.net>
2018-03-01 11:50:17 +0100

    Specs of model classes

---
+++ parkinsonProject/Models/AppointmentSet.swift
@@ -0,0 +1,104 @@
+//
+//  TreatmentSet.swift
+//  parkinsonProject
+//
+//  Created by Thierry WINTZ on 28/02/2018.
+//  Copyright © 2018 Mégane WINTZ. All rights reserved.
+//
+
+import Foundation
+
+/// AppointmentSet type
+///
+
+/// ---- Methods
+///
+
+/// init
+///
+/// initialize an 'AppointmentSet', empty.
+///
+
+
+/// addAppointment
+///
+/// add an Appointment to the set
+///
+/// - Parameters:
+///   - appointment: `Appointment`
+/// - Returns : 'AppointmentSet' with the appointment enter in parameter
+
+
+/// removeAppointment
+///
+/// remove an appointment to the set
+///
+/// - Parameters:
+///   - appointment: `Appointment`
+/// - Returns : 'AppointmentSet' without the appointment enter in parameter
+
+
+/// count
+///
+/// number of `Appointment` in the set
+///
+/// - Returns : 'Int'
+
+
+/// contains
+///
+///
+/// - Parameters:
+///   - appointment: `Appointment`
+/// - Returns : True if the appointment is in the set
+
+
+/// nextAppointment
+///
+/// give the next appointment programed
+///
+/// - Returns : Appointment
+
+
+/// updateAppointment
+///
+/// updates an appointment with a new value
+///
+/// - Parameters:
+///   - old: `Appointment`
+///   - new: Appointment
+/// - Returns : 'AppointmentSet' with the appointment updated
+
+
+/// checkConflict
+///
+/// return true if the appointment in parameter happens at the same time with another one in the set
+///
+/// - Parameters:
+///   - appointment: Appointment
+///
+/// Returns : true if the appointment in parameter happens at the same time with another one in the set
+
+
+/// addDelegate
+///
+/// add a delegate to this model
+///
+/// - Parameters:
+///   - delegate: `AppointmentSetDelegate`
+///
+/// Returns : 'AppointmentSet' the current instance with the delegate in parameter
+
+
+/// removeDelegate
+///
+/// remove a delegate to this model
+///
+/// - Parameters:
+///   - delegate: `AppointmentSetDelegate`
+///
+/// Returns : 'AppointmentSet' the current instance without the delegate in parameter
+
+class ActivitiesSet {
+    
+}
---
+++ parkinsonProject/Models/TreatmentSet.swift
@@ -0,0 +1,94 @@
+//
+//  TreatmentSet.swift
+//  parkinsonProject
+//
+//  Created by Thierry WINTZ on 28/02/2018.
+//  Copyright © 2018 Mégane WINTZ. All rights reserved.
+//
+
+import Foundation
+
+/// TreatmentSet type
+///
+
+/// ---- Methods
+///
+
+/// init
+///
+/// initialize an 'TreatmentSet', empty.
+///
+
+
+/// addTreatment
+///
+/// add a treatment to the set
+///
+/// - Parameters:
+///   - treatment: `Treatment`
+/// - Returns : 'TreatmentSet' with the treatment enter in parameter
+
+
+/// removeTreatment
+///
+/// remove a treatment to the set
+///
+/// - Parameters:
+///   - treatment: `Treatment`
+/// - Returns : 'TreatmentSet' without the treatment enter in parameter
+
+
+/// count
+///
+/// number of `Treatment` in the set
+///
+/// - Returns : 'Int'
+
+
+/// contains
+///
+///
+/// - Parameters:
+///   - treatment: `Treatment`
+/// - Returns : True if the treatment is in the set
+
+
+/// nextTreatments
+///
+/// give the next treatments programed (many treatments if they occurs at the same time)
+///
+/// - Returns : TreatmentSet
+
+
+/// updateTreatment
+///
+/// updates a treatment with a new value
+///
+/// - Parameters:
+///   - old: `Treatment`
+///   - new: Treatment
+/// - Returns : 'TreatmentSet' with the treatment updated
+
+
+/// addDelegate
+///
+/// add a delegate to this model
+///
+/// - Parameters:
+///   - delegate: `TreatmentSetDelegate`
+///
+/// Returns : 'TreatmentSet' the current instance with the delegate in parameter
+
+
+/// removeDelegate
+///
+/// remove a delegate to this model
+///
+/// - Parameters:
+///   - delegate: `TreatmentSetDelegate`
+///
+/// Returns : 'TreatmentSet' the current instance without the delegate in parameter
+
+class ActivitiesSet {
+    
+}
